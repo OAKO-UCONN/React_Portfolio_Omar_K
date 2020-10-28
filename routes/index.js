26a356ab8f3477a56eed0560d20ab449272da4f5
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-10-28 15:13:24 -0400

    Remove api requirement

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const path = require("path");
-const apiRoutes = require("./api");
+//const apiRoutes = require("./api");
 
-router.use("/api", apiRoutes);
+//router.use("/api", apiRoutes);
 
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
